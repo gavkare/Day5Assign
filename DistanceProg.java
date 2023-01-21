9770e09ddb033f9370a30b22cf562ad5a83a6d28
Shankar Gavkare <shankargavkare2277@gmail.com>
2023-01-21 14:11:51 +0530

    DistanceProg.java

---
+++ DistanceProg.java
@@ -0,0 +1,17 @@
+package Day5Assign;
+
+import java.util.Scanner;
+
+public class DistanceProg {
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            System.out.print("Enter x coordinate: ");
+            int x = input.nextInt();
+            System.out.print("Enter y coordinate: ");
+            int y = input.nextInt();
+            double distance = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+            System.out.println("Distance from point (" + x + ", " + y + ") to origin (0, 0) is " + distance);
+        }
+    }
+
+
