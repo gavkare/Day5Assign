5502912c5fc1174b3342b2d0cfb5caf9ed6d0ed9
Shankar Gavkare <shankargavkare2277@gmail.com>
2023-01-21 13:39:41 +0530

    FlipCoin

---
+++ FlipCoin.java
@@ -0,0 +1,28 @@
+package Day5Assign;
+
+import java.util.Random;
+
+public class FlipCoin {
+    public static void main(String[] args) {
+        int flips = 5;
+        int heads = 0;
+        int tails = 0;
+
+        Random rand = new Random();
+
+        for (int i = 0; i < flips; i++) {
+            double coin = rand.nextDouble();
+            if (coin < 0.5) {
+                tails++;
+            } else {
+                heads++;
+            }
+        }
+
+        double headsPercentage = (heads / (double) flips) * 100;
+        double tailsPercentage = (tails / (double) flips) * 100;
+
+        System.out.println("Heads: " + headsPercentage + "%");
+        System.out.println("Tails: " + tailsPercentage + "%");
+    }
+}
