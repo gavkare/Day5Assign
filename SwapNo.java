e571bd8989b5f49f506caae3a2a30daab992daa5
Shankar Gavkare <shankargavkare2277@gmail.com>
2023-01-21 13:58:49 +0530

    SwapNo.java

---
+++ SwapNo.java
@@ -0,0 +1,19 @@
+package Day5Assign;
+
+import java.util.Scanner;
+
+public class SwapNo {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter X and Y :" );
+        int x =sc.nextInt();
+        int y = sc.nextInt();
+
+        int temp = x;
+        x=y;
+        y=temp;
+        System.out.println("Swap Two Number: "+x+" "+y);
+
+
+    }
+}
