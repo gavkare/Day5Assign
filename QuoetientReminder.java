eae57c737857b194f9e7a0c7231fa3c3e469e38e
Shankar Gavkare <shankargavkare2277@gmail.com>
2023-01-21 13:57:12 +0530

    QuoetientReminder.java

---
+++ QuoetientReminder.java
@@ -0,0 +1,21 @@
+package Day5Assign;
+
+import java.util.Scanner;
+
+public class QuoetientReminder {
+    public static void main(String[] args)
+    {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("enter a number");
+
+        int dividend = sc.nextInt();
+        System.out.println("enter a divident");
+        int  d = sc.nextInt();
+
+        int quotient = dividend / d;
+        int remainder = dividend % d;
+
+        System.out.println("The Quotient is = " + quotient);
+        System.out.println("The Remainder is = " + remainder);
+    }
+}
